55507d3b6b9115c11807166079d7e3bcd7cb3858
Goksel Koksal <gokselkoksal@gmail.com>
2017-07-01 16:54:12 +0100

    Initial Commit

---
+++ Core/Core.h
@@ -0,0 +1,19 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Göksel Köksal on 01/07/2017.
+//  Copyright © 2017 GK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
