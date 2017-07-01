410fa311e48a34059f3086d01a461df66be35930
Goksel Koksal <gokselkoksal@gmail.com>
2017-07-01 18:48:10 +0100

    Add macOS tvOS watchOS targets

---
+++ Core/Supporting Files/Core-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Göksel Köksal on 01/07/2017.
+//  Copyright © 2017 GK. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Core watchOS.
+FOUNDATION_EXPORT double Core_watchOSVersionNumber;
+
+//! Project version string for Core watchOS.
+FOUNDATION_EXPORT const unsigned char Core_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core_watchOS/PublicHeader.h>
+
+
