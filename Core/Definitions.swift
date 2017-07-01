06eac8b0c691d70132381d75c92d5a405b387b57
Goksel Koksal <gokselkoksal@gmail.com>
2017-07-01 17:31:32 +0100

    Add all components

---
+++ Core/Definitions.swift
@@ -0,0 +1,20 @@
+//
+//  Definitions.swift
+//  Core
+//
+//  Created by Göksel Köksal on 04/06/2017.
+//  Copyright © 2017 GK. All rights reserved.
+//
+
+import Foundation
+
+protocol State { }
+
+protocol Action { }
+
+protocol NavigatorAction: Action { }
+
+protocol Command {
+    associatedtype StateType: State
+    func execute(on component: Component<StateType>, core: Core)
+}
